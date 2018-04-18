aaf5651eb9fb43ac69c1f6ed748ea082fb4aa770
Ji Yim <gsooyim@gmail.com>
2018-04-18 02:09:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   new_collection = []
   while i < collection.length 
-    yield collection[i]
+    new_collection << yield collection[i]
     i += 1
   end
 end
